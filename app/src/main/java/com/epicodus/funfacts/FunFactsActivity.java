fdec6d8e782ba285b1ce52dd2469d29ccc1c18f7
Kyle <kderrick1988@gmail.com>
2016-08-19 12:09:27 -0700

    save instance state of background color and fact

---
+++ app/src/main/java/com/epicodus/funfacts/FunFactsActivity.java
@@ -0,0 +1,64 @@
+package com.epicodus.funfacts;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+//    Declare our view variables
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private static final String KEY_FACT = "KEY_FACT";
+    private static final String KEY_COLOR = "KEY_COLOR" ;
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    private String mFact;
+    private int mColor;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(KEY_FACT, mFact);
+        outState.putInt(KEY_COLOR, mColor);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+//        Assigns the views from the layout to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFact = mFactBook.getFact();
+                mColor = mColorWheel.getColor();
+//                UPDATE FACT TO TEXTVIEW
+                mFactTextView.setText(mFact);
+                mRelativeLayout.setBackgroundColor(mColor);
+                mShowFactButton.setTextColor(mColor);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+//        Toast.makeText(FunFactsActivity.this, "YAY! OUR ACTIVITY WAS CREATED", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We are logging from the onCreate method");
+    }
+    
+}
